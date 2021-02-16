1c1d6456e41cad3c1d56d3106334286911caf6d2
closeplanet2 <filerjosh@hotmail.com>
2021-02-16 14:59:35 +0000

    Pump test 2.1

--- ArdunioTestCode/03_PumpController.ino
+++ ArdunioTestCode/03_PumpController.ino
@@ -5,43 +5,31 @@ void TriggerPump(int number){
       servoOne.write(90);
       delay(2000);
       servoOne.write(0);
-      delay(2000);
-      servoOne.write(90);
       break;
     case 2:
       servoTwo.write(90);
       delay(2000);
       servoTwo.write(0);
-      delay(2000);
-      servoTwo.write(90);
       break;
     case 3:
       servoThree.write(90);
       delay(2000);
       servoThree.write(0);
-      delay(2000);
-      servoThree.write(90);
       break;
     case 4:
       servoFour.write(90);
       delay(2000);
       servoFour.write(0);
-      delay(2000);
-      servoFour.write(90);
       break;
     case 5:
       servoFive.write(90);
       delay(2000);
       servoFive.write(0);
-      delay(2000);
-      servoFive.write(90);
       break;
     case 6:
       servoSix.write(90);
       delay(2000);
       servoSix.write(0);
-      delay(2000);
-      servoSix.write(90);
       break;
   }
 }
