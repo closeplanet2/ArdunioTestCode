13a76f45428c56397f20b862cd6c6ff650a95838
closeplanet2 <filerjosh@hotmail.com>
2021-02-16 11:39:21 +0000

    Created More tabs

--- ArdunioTestCode/01_Setup.ino
+++ ArdunioTestCode/01_Setup.ino
@@ -1,3 +1,10 @@
 void setup() {
+  Serial.begin(9600);
   
+  servoOne.attach(30);
+  servoTwo.attach(31);
+  servoThree.attach(32);
+  servoFour.attach(33);
+  servoFive.attach(34);
+  servoSix.attach(35);
 }
--- ArdunioTestCode/ArdunioTestCode.ino
+++ ArdunioTestCode/ArdunioTestCode.ino
@@ -1,1 +1,15 @@
+#include <Servo.h>
+#include <SoftwareSerial.h>
 
+SoftwareSerial mySerial(11, 12);
+
+Servo servoOne;
+Servo servoTwo;
+Servo servoThree;
+Servo servoFour;
+Servo servoFive;
+Servo servoSix;
+
+int distanceList[] = {100,80,60,40,20,0,0};
+int currentPump = -1;
+int currentDistance = 0;
